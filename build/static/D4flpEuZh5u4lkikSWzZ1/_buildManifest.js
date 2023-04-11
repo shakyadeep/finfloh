0fb229457d5a4e7265585d0e9bb14c9bef549e52
dkshakya18 <dkshakya2014@gmail.com>
2023-04-12 00:03:07 +0530

    build  remove

--- build/static/D4flpEuZh5u4lkikSWzZ1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/924-e342591c977ed5d2.js","static/chunks/pages/index-163e9045a5a7ab71.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
