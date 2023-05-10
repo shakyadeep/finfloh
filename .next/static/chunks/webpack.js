98bbe164a805dddd71e70f300e730d09e5c98aa4
dkshakya18 <dkshakya2014@gmail.com>
2023-05-10 15:58:24 +0530

    Section update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f8692a9bf1b9a0bb"; }
+/******/ 		__webpack_require__.h = function() { return "f7a08fac6eb26245"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
